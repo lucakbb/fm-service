caca9d0f757fcc33b269b4caccea040eb72a836b
Xiaozhe Yao <askxzyao@gmail.com>
2025-08-13 04:50:42 +0200

    update ocf-test.yaml

--- tests/test_llm_api.py
+++ tests/test_llm_api.py
@@ -2,10 +2,10 @@ import openai
 client = openai.OpenAI(
     api_key="-",
     # base_url="http://localhost:8080/v1",
-    base_url="https://fm.autoai.dev/v1",
+    base_url="http://148.187.108.173:8092/v1/service/llm/v1/",
 )
 response = client.chat.completions.create(
-    model="Qwen/Qwen3-8B",
+    model="Qwen/Qwen3-32B",
     messages=[
         {"role": "user", "content": "Hello, how are you?"},
     ]
